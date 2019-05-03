ddad23626ba648d1a5379c52337ba429fcaf2c4a
Taylor <Taylor@Taylors-MacBook-Pro-2.local>
2019-05-03 14:09:19 -0400

    5 question test

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,9 @@
 var friends = require("../data/friends");
 
-
-
 module.exports = function(app) {
-
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
-
   app.post("/api/friends", function(req, res) {
       friends.push(req.body);
       res.json(true);  
