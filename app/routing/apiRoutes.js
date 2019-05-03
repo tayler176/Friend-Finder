e6cf4e631200cbd86a3545b18946628a534a2fe6
Taylor <Taylor@Taylors-MacBook-Pro-2.local>
2019-05-03 12:10:05 -0400

    server test 3

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+var friends = require("../data/friends");
+
+
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+      friends.push(req.body);
+      res.json(true);  
+  });
+
+  app.post("/api/clear", function(req, res) {
+    friends.length = 0;
+        res.json({ ok: true });
+  });
+};
