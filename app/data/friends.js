791c7c3429bd7c345625ccfa8c97a01e03500a3c
Taylor <Taylor@Taylors-MacBook-Pro-2.local>
2019-05-06 16:04:07 -0400

    text 1

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,38 @@ var friendArray = [
             1
           ]
       },
+      {
+        "name":"small",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"big",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+          ]
+      },
 ];
 
 module.exports = friendArray
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,65 @@
 var friends = require("../data/friends");
+var friendScore = 0;
+var scoreArray = [];
+var diffArray = [];
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+// function bestFriendCalc() {
+//   for (var i = 0; i < friends.length; i++) {
+//     for (var j = 0; j < friends[i].scores.length; j++) {
+//       let score = parseInt(friends[i].scores[j]);
+//       friendScore += score;
+//     }
+//     scoreArray.push(friendScore);
+//     friendScore = 0;
+//   }
+//   var lastPerson = parseInt(scoreArray.length - 1)
+//   for (var k = 0; k < scoreArray.length - 1; k++) {
+//     var diffScore = parseInt(scoreArray[k] - scoreArray[lastPerson]);
+//     var absoluteDiffScore = Math.abs(diffScore)
+//     diffArray.push(absoluteDiffScore)
+//   }
+//   Array.min = function (diffArray) {
+//     return Math.min.apply(Math, diffArray);
+//   };
+//   var minimum = Array.min(diffArray);
+//   var indexOfMinimum = diffArray.indexOf(minimum)
+//   var bestFriend = friends[indexOfMinimum]
+//   res.json(bestFriend);
+//   console.log(res.json(bestFriend))
+// };
+
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
-  app.post("/api/friends", function(req, res) {
-      friends.push(req.body);
-      res.json(true);  
+  app.post("/api/friends", function (req, res) {
+    friends.push(req.body);
+    // bestFriendCalc();
+    for (var i = 0; i < friends.length; i++) {
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        let score = parseInt(friends[i].scores[j]);
+        friendScore += score;
+      }
+      scoreArray.push(friendScore);
+      friendScore = 0;
+    }
+    var lastPerson = parseInt(scoreArray.length - 1)
+    for (var k = 0; k < scoreArray.length - 1; k++) {
+      var diffScore = parseInt(scoreArray[k] - scoreArray[lastPerson]);
+      var absoluteDiffScore = Math.abs(diffScore)
+      diffArray.push(absoluteDiffScore)
+    }
+    Array.min = function (diffArray) {
+      return Math.min.apply(Math, diffArray);
+    };
+    var minimum = Array.min(diffArray);
+    var indexOfMinimum = diffArray.indexOf(minimum)
+    var bestFriend = friends[indexOfMinimum]
+    res.json(bestFriend);
+    console.log(res.json(bestFriend))
   });
-
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/clear", function (req, res) {
     friends.length = 0;
-        res.json({ ok: true });
+    res.json({ ok: true });
   });
 };
